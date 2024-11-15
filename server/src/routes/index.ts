765a2b4f788ae6b083f5e6edb8bff6b21b3a2c59
Cooper Hurt <40371983+cooperhurt@users.noreply.github.com>
2024-11-15 01:27:45 +0000

    Update Server Config

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
-import { data } from "../../data"
+import { data } from "../../data";
 const router = express.Router();
 
 // This is recommended to
 router.route("/").get(async (req, res) => {
-    return res.status(200).json({ message: "Default api endpoint" });
+  return res.status(200).json({ message: "Default api endpoint" });
 });
 
 router.route("/employees").get(async (req, res) => {
-    return res.status(200).json({ employees: data });
+  console.log("we made here here...");
+  return res.status(200).json({ employees: data });
 });
 
 export default router;
